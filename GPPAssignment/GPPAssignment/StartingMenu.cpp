201902bb493ff5d7e26521d5e6cdccea73679bb9
LKC-William <S10195561@connect.np.edu.sg>
2021-11-22 23:06:15 +0800

    Syncing

---
+++ GPPAssignment/GPPAssignment/StartingMenu.cpp
@@ -0,0 +1,22 @@
+#include "startingMenu.h"
+
+StartingMenu::StartingMenu()
+{
+	// Send Help
+
+}
+
+StartingMenu::~StartingMenu()
+{
+
+}
+
+void StartingMenu::initialize(HWND hwnd)
+{
+
+}
+
+void StartingMenu::update()
+{
+
+}
--- GPPAssignment/GPPAssignment/StartingMenu.h
+++ GPPAssignment/GPPAssignment/StartingMenu.h
@@ -1,1 +1,25 @@
+#ifndef _STARTINGMENU_H             // prevent multiple definitions if this 
+#define _STARTINGMENU_H             // ..file is included in more than one place
+#define WIN32_LEAN_AND_MEAN
 
+#include "textDX.h"
+#include "constants.h"
+
+//===================
+//MENU CLASS
+//===================
+class StartingMenu
+{
+protected:
+    TextDX* dxMenuText;
+public:
+    // Constructor
+    StartingMenu();
+
+    // Destructor
+    virtual ~StartingMenu();
+
+    void initialize(HWND hwnd);
+    void update();
+};
+#endif
