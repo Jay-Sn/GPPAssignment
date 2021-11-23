fe0e177497b16bb2d568324c2274a44235e87d6a
Jason Teo Jun Hao <jasonteo376@gmail.com>
2021-11-24 00:46:17 +0800

    Commit die

--- GPPAssignment/GPPAssignment/winmain.cpp
+++ GPPAssignment/GPPAssignment/winmain.cpp
@@ -47,7 +47,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
     MSG	 msg;
 
     //Create the game, sets up message handler
-    game = new PFantasy;
+    game = new Map;
 
     // Create the window
     if (!CreateMainWindow(hwnd, hInstance, nCmdShow))
